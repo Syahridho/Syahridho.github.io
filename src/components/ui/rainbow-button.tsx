e3c73cb2dfa3218d1d64e487898f0a80d7b1ed17
Syahridho Arjuna Syahputra <syahridhosyahputra@gmail.com>
2024-10-14 13:15:29 +0700

    add husky

--- src/components/ui/rainbow-button.tsx
+++ src/components/ui/rainbow-button.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-
 import { cn } from "@/lib/utils";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface RainbowButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
